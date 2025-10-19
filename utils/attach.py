36e873af2a444e49cbf86c2f111dc0c2ca2f52b2
SKropotova <Kropotova.S.M@yandex.ru>
2025-10-19 17:40:23 +0300

    fix

--- utils/attach.py
+++ utils/attach.py
@@ -1,50 +1,36 @@
-import os
 import allure
 
-def _video_base():
-    base = os.getenv('SELENOID_VIDEO_BASE') or os.getenv('VIDEO_HOST')
-    if base:
-        base = base.replace('http://', '').replace('https://', '').rstrip('/')
-        return f'https://{base}/video'
-    return 'https://selenoid.autotests.cloud/video'
+from allure_commons.types import AttachmentType
 
 
 def add_screenshot(browser):
-    try:
-        png = browser.driver.get_screenshot_as_png()
-        allure.attach(png, name='last_screenshot', attachment_type=allure.attachment_type.PNG)
-    except Exception:
-        pass
-
-
-def add_html(browser):
-    try:
-        html = browser.driver.page_source
-        allure.attach(html, name='page_source', attachment_type=allure.attachment_type.HTML)
-    except Exception:
-        pass
+    png = browser.driver.get_screenshot_as_png()
+    allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
 
 
 def add_logs(browser):
     try:
-        logs = '\n'.join([f"[{l['level']}] {l['message']}" for l in browser.driver.get_log('browser')])
-        if logs.strip():
-            allure.attach(logs, name='browser_console', attachment_type=allure.attachment_type.TEXT)
-    except Exception:
-        pass
+        if hasattr(browser.driver, 'get_log'):
+            logs = browser.driver.get_log(log_type='browser')
+            log_str = "\n".join(log['message'] for log in logs)
+        else:
+            log_str = "Browser logs not available (get_log not supported)"
+
+        allure.attach(body=log_str, name='browser_logs', attachment_type=AttachmentType.TEXT, extension='.log')
+    except Exception as e:
+        allure.attach(body=f"Error getting logs: {str(e)}", name='log_error', attachment_type=AttachmentType.TEXT,
+                      extension='.log')
+
+
+def add_html(browser):
+    html = browser.driver.page_source
+    allure.attach(body=html, name='page_source', attachment_type=AttachmentType.HTML, extension='.html')
 
 
 def add_video(browser):
-    try:
-        session_id = browser.driver.session_id
-        video_url = f"{_video_base()}/{session_id}.mp4"
-        html = (
-            "<html><body>"
-            f"<video width='100%' height='100%' controls autoplay>"
-            f"<source src='{video_url}' type='video/mp4'>"
-            "</video>"
-            "</body></html>"
-        )
-        allure.attach(html, name='video', attachment_type=allure.attachment_type.HTML)
-    except Exception:
-        pass
+    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
+    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
+           + video_url \
+           + "' type='video/mp4'></video></body></html>"
+    allure.attach(body=html, name='video: ' + browser.driver.session_id, attachment_type=AttachmentType.HTML,
+                  extension='.html')
