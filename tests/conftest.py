4b51ad1de1ed3dbcc8ae3a7518c4f5be3eab925a
SKropotova <Kropotova.S.M@yandex.ru>
2025-10-19 18:01:26 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,74 +1,45 @@
 import os
-import time
-import allure
 import pytest
+import allure
+from dotenv import load_dotenv
+
+from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+from utils import attach
 
-def _remote_url() -> str:
-    host = os.getenv("SELENOID_URL", "selenoid.autotests.cloud").strip()
-    login = os.getenv("SELENOID_LOGIN", "").strip()
-    password = os.getenv("SELENOID_PASS", "").strip()
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv(override=True)
 
-    if login and password:
-        return f"https://{login}:{password}@{host}/wd/hub"
-    return f"https://{host}/wd/hub"
+@pytest.fixture(scope='function', autouse=True)
+def remote_browser_setup():
+    login = os.getenv('SELENOID_LOGIN')
+    password = os.getenv('SELENOID_PASS')
+    host = os.getenv('SELENOID_URL', '')
+    assert all([login, password, host]), 'ENV missing'
 
+    host = host.replace('http://', '').replace('https://', '').rstrip('/')
 
-def _video_base() -> str:
-    return os.getenv("SELENOID_VIDEO_BASE", "https://selenoid.autotests.cloud/video").rstrip("/")
-
-
-@pytest.fixture(scope="session")
-def browser_name():
-    return os.getenv("BROWSER", "chrome").lower()
-
-
-@pytest.fixture(scope="session")
-def browser_version():
-    return os.getenv("BROWSER_VERSION", "128.0")
-
-
-@pytest.fixture
-def browser(request, browser_name, browser_version):
     options = Options()
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-
-    capabilities = {
-        "browserName": "chrome",
-        "browserVersion": browser_version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True,
-            "videoFrameRate": 24,
-            "screenResolution": "1920x1080x24",
-        },
-    }
+    options.set_capability('browserName', 'firefox')
+    options.set_capability('browserVersion', '125.0')
+    options.set_capability('selenoid:options', {'enableVNC': True, 'enableVideo': True})
+    options.set_capability('goog:loggingPrefs', {'browser': 'ALL'})
 
     driver = webdriver.Remote(
-        command_executor=_remote_url(),
-        options=options,
-        desired_capabilities=capabilities
+        command_executor=f'https://{login}:{password}@{host}/wd/hub',
+        options=options
     )
+    browser.config.driver = driver
 
-    time.sleep(1)
-
-    yield driver
-
-    session_id = driver.session_id
-    driver.quit()
-
-    time.sleep(1)
-
-    video_url = f"{_video_base()}/{session_id}.mp4"
-    html = (
-        f"<html><body>"
-        f"<video width='100%' height='100%' controls autoplay>"
-        f"<source src='{video_url}' type='video/mp4'>"
-        f"</video>"
-        f"</body></html>"
-    )
-    allure.attach(html, name="Selenoid video", attachment_type=allure.attachment_type.HTML)
+    try:
+        yield browser
+    finally:
+        with allure.step('Tear down'):
+            attach.add_screenshot(browser)
+            attach.add_logs(browser)
+            attach.add_html(browser)
+            attach.add_video(browser)
+        browser.quit()
